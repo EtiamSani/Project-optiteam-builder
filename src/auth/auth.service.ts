fd9e354063199e3cb9aaa74c768a9fe055762f9e
EtiamSani <etiam.sani@laposte.net>
2023-08-22 11:39:59 +0200

    auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService) {
 
     }
-    signup(dto : AuthDto) {
-        return {msg: 'hello'}
+    async signup(dto : AuthDto) {
+        const password = await argon.hash(dto.password);
+        const username = dto.username;
+        try {
+            
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    password,
+                    username,
+                },
+    
+            });
+            delete user.password
+            
+            return user;
+        } catch(error) {
+            // récupérer les erreurs envoyé par prisma
+            if (error instanceof PrismaClientKnownRequestError) { 
+                // Code erreur spécifique pour signaler quand un champs unique a été violé 
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken'
+                    );
+                }
+            }
+            throw error;
+        }
     }
 
     signin() {
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  })); // whiteliste a true permet de filtrer les données c-à-d accepter uniquement des données dans le body qui ont été définis dans le DTO
   await app.listen(3000);
 }
 bootstrap();
