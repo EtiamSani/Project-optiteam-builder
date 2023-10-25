a1ff99a5beadee9ce60979012cc811c82c3a75e7
EtiamSani <etiam.sani@laposte.net>
2023-10-25 11:27:10 +0200

    fix google auth bugs

--- src/auth/google-oauth/google-oauth.service.ts
+++ src/auth/google-oauth/google-oauth.service.ts
@@ -12,52 +12,19 @@ export class GoogleOauthService {
   constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService, private errorHandlerService: ErrorHandlerService) {
   }
 
-//   async googleSignup(dto : AuthDto) {
-//     const password = await argon.hash(dto.password); 
-//     try {
-        
-//         const user = await this.prisma.user.create({
-//             data: {
-//                 email: dto.email,
-//                 password,
-//                 username :dto.username,
-//                 teamId: dto.teamId
-//             },
-
-//         });
-//         return this.signToken(user.id, user.email, user.teamId);
-//     } catch(error) {
-//         // récupérer les erreurs envoyé par prisma
-//         if (error instanceof PrismaClientKnownRequestError) { 
-//             // Code erreur spécifique pour signaler quand un champs unique a été violé 
-//             this.errorHandlerService.handle(error);
-//         }
-//         throw error;
-//     }
-// }
-
-// async googleLogin(req , res) {
-
-// res.json(req.user.googleAccesstoken)
-
-// console.log(req.user.googleAccesstoken)
-
-//   }
-async googleLogin(req, res) {
+  async googleLogin(req, res) {
   // Récupérez le jeton d'accès
-  const accessToken = req.user.googleAccesstoken;
-  console.log(accessToken)
+  const googleAccessToken = req.user.googleAccesstoken;
+  console.log(googleAccessToken)
 
-  if (!accessToken) {
+  if (!googleAccessToken) {
     return res.status(401).json({ message: 'No access token found' });
   }
 
   try {
-    // Décoder le jeton d'accès
-    const user = req.user
-    console.log(user)
 
-    // Extrayez les informations telles que le nom et l'e-mail
+    const user = req.user
+   
     const { email, username } = user;
 
      // Vérifiez si l'utilisateur existe dans la base de données
@@ -68,11 +35,12 @@ async googleLogin(req, res) {
     });
 
     if (!existingUser) {
-      // L'utilisateur n'existe pas, créez-le
+    
       existingUser = await this.googleSignup({ email, username });
       return res.status(200).json({ message: 'User information saved' });
     } else {
-      return res.status(200).json({ message: 'User connected', accessToken});
+      res.setHeader('Authorization', 'Bearer ' + googleAccessToken);
+      return res.status(200).json({ message: 'User connected' });
     }
     
   } catch (error) {
@@ -88,11 +56,9 @@ async googleSignup(data) {
       data: {
         email: data.email,
         password,
-        username: data.username, // You can change this to another value as needed
+        username: data.username, 
       },
     });
-
-    // You may want to return the user or sign a JWT for the user here
    return user
   } catch (error) {
     // Handle any errors that occur during user creation
